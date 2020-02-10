a1375460c3e06c54861fc6ad2406524895b79c0d
Bart van Moorsel <bartvanmoorsel7@gmail.com>
2020-02-10 13:31:38 +0100

    Main now launches gui and buttons are now all  addressable

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,34 @@
-public class Main {
-    private boolean test;
+import gui.Gui;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+
+        try {
+            Parent root = FXMLLoader.load(Gui.class.getResource("Application.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+//        System.out.println("done");
+    }
 }
